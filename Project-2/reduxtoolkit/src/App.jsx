00cb4ba38367d6d026da4255c9512a4ef73691ad
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-09-20 23:55:20 +0530

    Project-2

---
+++ Project-2/reduxtoolkit/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import AddTodo from '../src/Components/AddTodo'
+import Todo from '../src/Components/Todo'
+
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+
+<AddTodo />
+<Todo/>
+
+    </>
+  )
+}
+
+export default App
