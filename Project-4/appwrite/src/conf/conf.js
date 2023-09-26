d6293dd2c9bc925ab0b93f42a8a46b5da65c9c4d
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-09-26 22:41:01 +0530

    Project-4

--- Project-4/appwrite/src/conf/conf.js
+++ Project-4/appwrite/src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf={
     appwriteUrl:String(import.meta.env.VITE_APP_APPWRITE_URL),
-    appwriteProjectId:(import.meta.env.VITE_APP_APPWRITE_PROJECT_ID),
-    appwriteCollectionId:(import.meta.env.VITE_APP_APPWRITE_COLLECTION_ID),
-    appwriteDatabaseId:(import.meta.env.VITE_APP_APPWRITE_DATABASE_ID),
-    appwriteBucketId:(import.meta.env.VITE_APP_APPWRITE_BUCKET_ID),
+    appwriteProjectId:String(import.meta.env.VITE_APP_APPWRITE_PROJECT_ID),
+    appwriteCollectionId:String(import.meta.env.VITE_APP_APPWRITE_COLLECTION_ID),
+    appwriteDatabaseId:String(import.meta.env.VITE_APP_APPWRITE_DATABASE_ID),
+    appwriteBucketId:String(import.meta.env.VITE_APP_APPWRITE_BUCKET_ID),
 }
 
 
